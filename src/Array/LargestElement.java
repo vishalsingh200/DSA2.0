903f42fc9da29ec6efcdc3223158048bc998a682
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-11-27 14:01:10 +0530

    Initial commit

--- src/Array/LargestElement.java
+++ src/Array/LargestElement.java
@@ -13,7 +13,6 @@ public class LargestElement {
         int res = Largest(arr);
         System.out.println(res);
     }
-    //
     public static int Largest(int[] arr){
         int largest = 0;
         for (int i = 0; i < arr.length; i++){
