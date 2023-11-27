e0dac210a94f596e37d2f7a86442a3d2c36f84ec
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2023-11-27 12:25:30 +0530

    Initial commit

---
+++ src/Array/LargestElement.java
@@ -0,0 +1,26 @@
+package Array;
+import java.util.*;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //arr = 1 2 3 4 o/p = 4 otherwise return -1
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int res = Largest(arr);
+        System.out.println(res);
+    }
+    //
+    public static int Largest(int[] arr){
+        int largest = 0;
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] >= largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+}
